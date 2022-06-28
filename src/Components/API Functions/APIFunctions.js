c30e2471ac67991c8a02b6cfb902489f09eaaa1a
Gabriel Theodoro Ferreira <gabthefer1@gmail.com>
2022-06-28 20:50:47 -0300

    vo joga um fut kkkkk

--- src/Components/API Functions/APIFunctions.js
+++ src/Components/API Functions/APIFunctions.js
@@ -1,5 +1,7 @@
 import axios from "axios"
-const auth = {headers: {Authorization: "809503c1-0082-48d9-a9f3-7c1b054ffb8c"}}
+const auth = {headers: {Authorization: "809503c1-0082-48d9-a9f3-7c1b054ffb8c"}} //auth do grupo
+const demoauth={headers: {Authorization:"e2190c39-7930-4db4-870b-bed0e5e4b88e"}}
+
 export const CreateJob = (title,description,price,paymentMethods,dueDate) => {
     console.log('alou')
     const url = "https://labeninjas.herokuapp.com/jobs"
@@ -10,7 +12,7 @@ export const CreateJob = (title,description,price,paymentMethods,dueDate) => {
         paymentMethods: paymentMethods,
         dueDate: dueDate
     }
-    axios.post(url, body, auth).then((res=>{
+    axios.post(url, body, demoauth).then((res=>{
         console.log(res)
     })).catch(err=>{alert(err.message)}) 
 }
