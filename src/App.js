ad0b50d8b8dac278c4e8e2e0a42699ee70722db8
Gabriel Theodoro Ferreira <gabthefer1@gmail.com>
2022-06-30 19:13:47 -0300

    SAMURAIZUDO

--- src/App.js
+++ src/App.js
@@ -6,34 +6,35 @@ import ShopCart from "./Components/ShopCart/ShopCart";
 import Header from "./Components/Header/Header";
 
 export default class App extends React.Component {
-  
   state = {
     actualScreen: "jobList",
-    carrinho:[],
+    carrinho: [],
   };
-  addServices = (info,name,jobs)=>{
-    const filterServices = jobs.filter((dados)=>{
-      return dados.id === info
-    })
-    const cartCopy = [...this.state.carrinho]
-    const cartControl = cartCopy.map((item)=>{
-      return item.id
-    }).find((dado)=>{
-      return dado === info
-    })
-    if(cartControl===undefined){
-      cartCopy.push(filterServices[0])
-      this.setState({carrinho:cartCopy})
-    }else{
-      alert(`O serviço ${name} já foi adicionado.`)
+  addServices = (info, name, jobs) => {
+    const filterServices = jobs.filter((dados) => {
+      return dados.id === info;
+    });
+    const cartCopy = [...this.state.carrinho];
+    const cartControl = cartCopy
+      .map((item) => {
+        return item.id;
+      })
+      .find((dado) => {
+        return dado === info;
+      });
+    if (cartControl === undefined) {
+      cartCopy.push(filterServices[0]);
+      this.setState({ carrinho: cartCopy });
+    } else {
+      alert(`O serviço ${name} já foi adicionado.`);
     }
-  }
-  removeService = (id)=>{
-    const deleteService = this.state.carrinho.filter((dados)=>{
-      return dados.id !== id
-    })
-    this.setState({carrinho:deleteService})
-  }
+  };
+  removeService = (id) => {
+    const deleteService = this.state.carrinho.filter((dados) => {
+      return dados.id !== id;
+    });
+    this.setState({ carrinho: deleteService });
+  };
   changeScreen = () => {
     switch (this.state.actualScreen) {
       case "landpage":
@@ -41,9 +42,21 @@ export default class App extends React.Component {
       case "jobRegister":
         return <JobRegister />;
       case "jobList":
-        return <JobList carrinho={this.state.carrinho} addServices={this.addServices} removeService={this.removeService} goTo={this.goTo} />;
+        return (
+          <JobList
+            carrinho={this.state.carrinho}
+            addServices={this.addServices}
+            removeService={this.removeService}
+            goTo={this.goTo}
+          />
+        );
       case "shopCart":
-        return <ShopCart carrinho={this.state.carrinho} removeService={this.removeService}/>;
+        return (
+          <ShopCart
+            carrinho={this.state.carrinho}
+            removeService={this.removeService}
+          />
+        );
     }
   };
 
--- src/Components/JobDetails/JobDetails.js
+++ src/Components/JobDetails/JobDetails.js
@@ -4,11 +4,15 @@ import {
   Container,
   Containerzudo,
   Description,
-  BackDetails, 
+  BackDetails,
   PaymentDiv,
-  MethodsCard
+  MethodsCard,
+  MethodsDiv,
+  ValueAndDate,
+  InfosDiv,
 } from "../JobDetails/styled";
 import Cart from "../img/iconcart.png";
+import CartPreto from "../img/iconcartpreto.png";
 
 const demoauth = {
   headers: { Authorization: "e2190c39-7930-4db4-870b-bed0e5e4b88e" },
@@ -29,7 +33,7 @@ export default class JobDetails extends React.Component {
     axios
       .get(url, demoauth)
       .then((res) => {
-        console.log(this.state.jobInfos)
+        console.log(this.state.jobInfos);
         this.setState({ jobInfos: res.data });
       })
       .catch((error) => {
@@ -38,21 +42,54 @@ export default class JobDetails extends React.Component {
   };
 
   render() {
-    const paymentMethods = this.state.jobInfos.paymentMethods?.map((dados, index) => {
-      return <MethodsCard key={index}>{dados.toUpperCase()}</MethodsCard>
-    })
+    const paymentMethods = this.state.jobInfos.paymentMethods?.map(
+      (dados, index) => {
+        return <MethodsCard key={index}>{dados.toUpperCase()}</MethodsCard>;
+      }
+    );
     return (
       <Containerzudo>
         <Container>
           <h1>{this.state.jobInfos.title?.toUpperCase()}</h1>
           <Description>{this.state.jobInfos.description}</Description>
-          <PaymentDiv> Métodos de pagamento: {paymentMethods}</PaymentDiv>
-          <h3>R$ {this.state.jobInfos.price} </h3>
-          <h2>{`Data de expiração: ${this.state.jobInfos.dueDate?.slice(8, 10)}/${this.state.jobInfos.dueDate?.slice(5, 7)}/${this.state.jobInfos.dueDate?.slice(0, 4)}`}
-          </h2>
+          <PaymentDiv>
+            Métodos de pagamento
+            <MethodsDiv>{paymentMethods}</MethodsDiv>
+          </PaymentDiv>
+          <ValueAndDate>
+            <InfosDiv>
+              <p>Valor Total</p>
+              <h2>${this.state.jobInfos.price} </h2>
+            </InfosDiv>
+
+            <InfosDiv>
+              <p>Data de expiração</p>
+              <h2>
+                {`${this.state.jobInfos.dueDate?.slice(
+                  8,
+                  10
+                )}/${this.state.jobInfos.dueDate?.slice(
+                  5,
+                  7
+                )}/${this.state.jobInfos.dueDate?.slice(0, 4)}`}
+              </h2>
+            </InfosDiv>
+          </ValueAndDate>
           <BackDetails>
+            <div
+            onClick={() =>
+              this.props.addServices(
+                this.state.jobInfos.id,
+                this.state.jobInfos.title,
+                this.props.jobs
+              )
+            }>
+              <p>Adicionar ao Carrinho</p>
+              <img               
+                src={CartPreto}
+              />
+            </div>
             <p onClick={this.props.closePopUp}>Voltar</p>
-            <img onClick={()=>this.props.addServices(this.state.jobInfos.id, this.state.jobInfos.title,this.props.jobs)} src={Cart} />
           </BackDetails>
         </Container>
       </Containerzudo>
