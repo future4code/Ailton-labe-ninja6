8f5a86cdb0026ed8ab66b7e1a92af941f04c8faf
Gabriel Theodoro Ferreira <gabthefer1@gmail.com>
2022-07-02 16:36:32 -0300

    fiz tela de loading

---
+++ src/Components/Loading Screen/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+width: 100%;
+height: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+img {
+    max-width: 2rem;
+}
+`
+
+export const LoadText = styled.h1 `
+display: flex;
+align-items: center;
+justify-content: center;
+font-size: 2rem;
+font-size: bolder;
+width: fit-content;
+height: fit-content;
+padding: 4px;
+border-radius: 8px;
+background-color: #303030;
+color: #ffff;
+`
